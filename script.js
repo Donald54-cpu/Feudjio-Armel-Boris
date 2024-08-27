c60bbe363bfc5ca70234682a0cc65113a1135631
Donald <alainmedougou@gmail.com>
2024-08-27 17:00:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+<div class="contact-item">
+<a href="https://www.facebook.com" target="_blank">
+    <i class="fab fa-facebook" style="color: #1877F2;"></i> <!-- Icône Facebook -->
+</a>
+</div>
+
